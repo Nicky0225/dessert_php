41af0b72bef8c91b668bc85c80c42f3c9472fe30
HSIN I HUANG <nickyhuang613@gmail.com>
2024-08-30 12:59:41 +0800

    zz

--- resources/views/cart/listUserCart.blade.php
+++ resources/views/cart/listUserCart.blade.php
@@ -10,7 +10,7 @@
         <!-- <h1>{{ $title }}</h1> -->
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <!-- Bootstrap CSS -->
-        <!-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"> -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -18,7 +18,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 
         <!-- Bootstrap JS -->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
