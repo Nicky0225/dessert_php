cb45dfbe9664f0ea7964d56683efdfa0d7cace07
HSIN I HUANG <nickyhuang613@gmail.com>
2024-09-07 17:07:35 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -100,7 +100,7 @@ Route::post('/update-cart-quantity/{id}', 'App\Http\Controllers\CartcheckControl
 
 Route::post('/place-order', 'App\Http\Controllers\CartcheckController@placeOrder')->name('placeOrder');
 
-Route::get('/.well-known/cf-2fa-verify.txt', 'App\Http\Controllers\Cloudflare@Cloudflare2FA')->name('authy');
+// Route::get('/.well-known/cf-2fa-verify.txt', 'App\Http\Controllers\Cloudflare@Cloudflare2FA')->name('authy');
 Route::get('/.well-known/cf-2fa-verify', 'App\Http\Controllers\Cloudflare@Cloudflare2FA')->name('authy');
 
 Route::get('/', function () {
